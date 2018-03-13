eb754cfa9aac3fc3f8fe691caeec1278e2b4567f
Nicholas Johnston <nicholas.johnston94@gmail.com>
2018-03-13 09:17:42 -0700

    Squashed commit messages, added authors and CRS description

---
+++ config/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+export const colors = StyleSheet.create({
+  shadow: "#f1f1f1",
+  vanilla: "#f3e8bb",
+  tangerine: "#fbb756",
+  juicy: "#f18b01",
+  peachy: "#ee896b",
+  lilac: "#ab627d",
+  night: "#323e54"
+});
