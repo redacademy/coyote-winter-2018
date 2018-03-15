bb3e5b96f679e86e2724a51971ce93e3f789c6e4
Lindsey <lindseywoo@gmail.com>
2018-03-14 23:25:12 -0700

    deleted unnecessary proptypes in the container file
    implemented interface for search location page and query helpers
    
    fixed merge conflict
    
    more styling and added logo
    
    added helper function
    
    changed styling until fonts are linked
    
    added proptypes to the container and refactored
    
    refactored LocationSearchContainer to feed into a stateless component, proptypes added, style comments made, stray imports removed'
    
    separated LocationSearchContainer to have a stateless component and added proptypes
    
    merge conflict
    
    added proptypes to container
    
    removed proptype in container

--- js/config/firebaseConfig.js
+++ js/config/firebaseConfig.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
 import 'firebase/auth';
+<<<<<<< HEAD
 import {
   API_KEY,
   AUTH_DOMAIN,
@@ -17,9 +18,25 @@ const config = {
   projectId: PROJECT_ID,
   storageBucket: STORAGE_BUCKET,
   messagingSenderId: MESSAGING_SENDING_ID
+=======
+import 'firebase/firestore';
+
+// Initialize firebase app
+const config = {
+  apiKey: 'AIzaSyCieBwguDuYharx8U99VYfVxreXAEfy3RA',
+  authDomain: 'coyote-2018.firebaseapp.com',
+  databaseURL: 'https://coyote-2018.firebaseio.com',
+  projectId: 'coyote-2018',
+  storageBucket: 'coyote-2018.appspot.com',
+  messagingSenderId: '672612380925'
+>>>>>>> implemented interface for search location page and query helpers
 };
 
 const firebaseApp = firebase.initializeApp(config);
 const firebaseAuth = firebaseApp.auth();
 const firestoreDb = firebase.firestore();
+<<<<<<< HEAD
 export { firebaseAuth, firestoreDb };
+=======
+export { firebaseApp, firebaseAuth, firestoreDb };
+>>>>>>> implemented interface for search location page and query helpers
---
+++ js/config/helpers.js
@@ -0,0 +1,25 @@
+import { firestoreDb } from './firebaseConfig';
+
+// returns a promise for the query for the given userId
+// used to retrieve data from the users collection
+export const getUserProfile = userId => {
+  const db = firestoreDb.collection('users').doc(userId);
+
+  return db.get();
+};
+
+// userData should be an object of key/values pairs corresponding
+// to database fields that should be updated
+// - example of usage:
+//  updateUserProfile('20032OjyKweGTpikv65HoUeQCQr1', {location: 'Scranton, PA'});
+//
+export const updateUserProfile = (userId, userData) => {
+  const db = firestoreDb.collection('users').doc(userId);
+  return db.update(userData);
+};
+
+export const getListingsByLocation = location => {
+  const db = firestoreDb.collection('listings').where('city', '==', location);
+
+  return db.get();
+};
