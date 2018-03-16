f6c5c5ecb7976827d1882ccfca31c0d21f1d432f
Colin MJ <colinmj@telus.net>
2018-03-16 16:57:52 -0700

    Deleted comment

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@
  */
 
 import React, { Component } from "react";
-// import { Text, View } from "react-native";
 import Login from "./screens/Login";
 import { Provider } from "react-redux";
 import Store from "./redux/store";
