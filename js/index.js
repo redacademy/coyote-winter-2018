dfb1624ca96720095c73e4a47dacda8116e18ef2
Colin MJ <colinmj@telus.net>
2018-03-22 11:18:20 -0700

    Added keyboard adjustment in location search page

--- js/index.js
+++ js/index.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import Store from './redux/store';
 import { Provider } from 'react-redux';
 import FilteringScreen from './screens/Filtering';
+import LocationSearch from './screens/LocationSearch';
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={Store}>
-        <FilteringScreen />
+        <LocationSearch />
       </Provider>
     );
   }
